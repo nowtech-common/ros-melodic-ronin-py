df3e87a7fadf599c4cef49536d4782681c501bae
Balazs Bamer <balazs@nowtech.hu>
2019-10-30 14:28:20 +0100

    The source publishes messages.

--- src/circle-test-source/scripts/talker.py
+++ src/circle-test-source/scripts/talker.py
@@ -1,21 +1,50 @@
 #!/usr/bin/env python3
 # license removed for brevity
+import math
 import rospy
+import std_msgs.msg
 from sensor_msgs.msg import Imu
 
-def talker():
-    pub = rospy.Publisher('ronin_imu', String, queue_size=10)
+def talker(aArgs):
+    pub = rospy.Publisher('ronin_imu', Imu, queue_size=10)
     rospy.init_node('circle_test_source', anonymous=True)
-    rate = rospy.Rate(200) # hz
+    rate = rospy.Rate(aArgs.sampling_freq) # Hz
+    step = 0
+    imu = Imu()
+    imu.orientation_covariance = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    imu.angular_velocity_covariance = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    imu.linear_acceleration_covariance = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    imu.orientation.x = 0.0
+    imu.orientation.y = 0.0
+    imu.orientation.z = 0.0
+    imu.orientation.w = 0.0
+    imu.angular_velocity.x = 0.0
+    imu.angular_velocity.y = 0.0
+    imu.linear_acceleration.z = 0.0
     while not rospy.is_shutdown():
-# TODO replace with real logic
-        hello_str = "hello world %s" % rospy.get_time()
-        rospy.loginfo(hello_str)
-        pub.publish(hello_str)
+        imu.header.seq = step
+        imu.header.stamp = rospy.Time.now()
+        angle = 2.0 * math.pi * aArgs.envelope_freq / aArgs.sampling_freq * step
+        omega = aArgs.omega_max * (1.0 - math.cos(angle)) / 2.0
+        imu.angular_velocity.z = omega
+        imu.linear_acceleration.x = math.pi * aArgs.envelope_freq * aArgs.omega_max * math.sin(angle) * aArgs.radius
+        imu.linear_acceleration.y = omega * omega * aArgs.radius
+        pub.publish(imu)
+        step = step + 1
         rate.sleep()
 
 if __name__ == '__main__':
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--radius', type=float, default=1.0)
+    parser.add_argument('--omega_max', type=float, default=1.0)
+    parser.add_argument('--sampling_freq', type=int, default=200)
+    parser.add_argument('--envelope_freq', type=float, default=0.5 / math.pi)
+
+    args = parser.parse_args()
+
     try:
-        talker()
+        talker(args)
     except rospy.ROSInterruptException:
         pass
